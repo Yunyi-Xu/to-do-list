7e788938c92e839d3ba3ad0f563fc6aaabba7a79
Yunyi <yunyixu@uvic.ca>
2023-03-27 01:09:54 -0700

    added clear all function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,18 @@ import Item from "./Item";
 
 function App() {
   const [inputText, setInputText] = useState("");
-  const [toDoList, setToDoList] = useState(["Click me to strikethrough", "Click again to delete :D"]);
+  const [toDoList, setToDoList] = useState(["Click to strikethrough", "Click again to un-do"]);
   function handleChange(event){
     setInputText(event.target.value);
   }
   function handleClick(){
+    
     setToDoList([...toDoList, inputText]);
     setInputText("");
 
+  }
+  function handleClear(){
+    setToDoList([]);
   }
   function deleteItem(id) {
     setToDoList(prevItems => {
@@ -22,8 +26,9 @@ function App() {
   
   return (
     <div className="container">
-      <div className="heading">
+      <div className="heading" style={{ display: "flex" }}> 
         <h1>To-Do List</h1>
+        <button onClick={handleClear} style={{marginLeft: 'auto'}}>Clear All</button>
       </div>
       <div className="form">
         <input type="text" onChange={handleChange} value={inputText}/>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,21 +3,21 @@ import React, {useState} from "react";
 function Item(props){
   const [isCrossed, setCrossed] = useState(false);
   function handleClick(){
-    console.log(props.id);
     if (!isCrossed){
       setCrossed(true);
       
     }
     else{
       setCrossed(false);
-      props.onChecked(props.id);
+      /* props.onChecked(props.id); */
     }
     
   }
   return (
     <li 
+      style={{textDecorationLine: isCrossed ? "line-through":"none"}}
       onClick={handleClick}
-      style={{textDecorationLine: isCrossed ? "line-through":"none"}}>
+      >
       {props.value}
     </li>
   );
